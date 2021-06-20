6bebdd14dd7b71853f789047258fb55647775ed0
sang-w0o <robbyra@gmail.com>
2021-06-21 08:07:43 +0900

    Final cleanup

--- src/main/kotlin/com/template/Application.kt
+++ src/main/kotlin/com/template/Application.kt
@@ -11,27 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootApplication
 @EnableJpaAuditing
-//@EnableCaching
 class Application {
     @Bean
     fun passwordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder(10)
     }
-
-//    @Bean
-//    fun cacheConfiguration(): RedisCacheConfiguration {
-//        return RedisCacheConfiguration.defaultCacheConfig()
-//            .entryTtl(Duration.ofMinutes(60)) // 1시간의 TTL 지정
-//            .disableCachingNullValues()  // Null 값에 대한 caching disable
-//            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer()))
-//    }
-//
-//    @Bean
-//    fun redisCacheManagerBuilder(): RedisCacheManagerBuilderCustomizer {
-//        return RedisCacheManagerBuilderCustomizer { builder ->
-//            builder.withCacheConfiguration("userCache", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
-//        }
-//    }
 
     @Bean
     fun userRedisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, User> {
