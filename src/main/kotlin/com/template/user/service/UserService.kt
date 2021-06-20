6805d2bf10a242041e0716df8b1108b3655bc82d
sang-w0o <robbyra@gmail.com>
2021-06-20 15:53:33 +0900

    Create service code

--- src/main/kotlin/com/template/user/service/UserService.kt
+++ src/main/kotlin/com/template/user/service/UserService.kt
@@ -1,13 +1,18 @@
 package com.template.user.service
 
 import com.template.auth.tools.JwtTokenUtil
+import com.template.common.function.FindUser
 import com.template.domain.user.UserRepository
+import com.template.user.dto.UserInfoResponseDto
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class UserService(private val jwtTokenUtil: JwtTokenUtil, private val userRepository: UserRepository) {
+class UserService(private val jwtTokenUtil: JwtTokenUtil, private val userRepository: UserRepository, private val findUser: FindUser) {
 
     @Transactional(readOnly = true)
-    fun getUserInfo(dto)
+    fun getUserInfo(): UserInfoResponseDto {
+        val user = findUser.get()
+        return UserInfoResponseDto(user)
+    }
 }
