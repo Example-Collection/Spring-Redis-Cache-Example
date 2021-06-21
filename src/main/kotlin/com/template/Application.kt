1bd63e2699844402cbdb7cc3de864369c1b1ce28
sang-w0o <robbyra@gmail.com>
2021-06-22 08:12:23 +0900

    - Main Application에 Cache 관련 설정

--- src/main/kotlin/com/template/Application.kt
+++ src/main/kotlin/com/template/Application.kt
@@ -1,48 +1,43 @@
 package com.template
 
-import com.template.domain.user.User
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import java.time.Duration
 
 @SpringBootApplication
 @EnableJpaAuditing
-//@EnableCaching
+@EnableCaching
 class Application {
     @Bean
     fun passwordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder(10)
     }
 
-//    @Bean
-//    fun cacheConfiguration(): RedisCacheConfiguration {
-//        return RedisCacheConfiguration.defaultCacheConfig()
-//            .entryTtl(Duration.ofMinutes(60)) // 1시간의 TTL 지정
-//            .disableCachingNullValues()  // Null 값에 대한 caching disable
-//            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-//                GenericJackson2JsonRedisSerializer()
-//            ))
-//    }
-//
-//    @Bean
-//    fun redisCacheManagerBuilder(): RedisCacheManagerBuilderCustomizer {
-//        return RedisCacheManagerBuilderCustomizer { builder ->
-//            builder.withCacheConfiguration("userCache", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
-//        }
-//    }
+    @Bean
+    fun cacheConfiguration(): RedisCacheConfiguration {
+        return RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(60)) // 1시간의 TTL 지정
+            .disableCachingNullValues()  // Null 값에 대한 caching disable
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                GenericJackson2JsonRedisSerializer()
+            ))
+    }
 
-//    @Bean
-//    fun userRedisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, User> {
-//        val redisTemplate = RedisTemplate<String, User>()
-//        redisTemplate.connectionFactory = redisConnectionFactory
-//        return redisTemplate
-//    }
+    @Bean
+    fun redisCacheManagerBuilder(): RedisCacheManagerBuilderCustomizer {
+        return RedisCacheManagerBuilderCustomizer { builder ->
+            builder.withCacheConfiguration("blogCache", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
+        }
+    }
 
 }
 fun main(args: Array<String>) {
